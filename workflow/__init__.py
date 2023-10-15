469cd55ef4c823ae2638b5428ab344b42a71dda4
turhanberk <turhanberkwb@gmail.com>
2023-10-15 01:12:51 -0400

    init.py author and mail updates

--- workflow/__init__.py
+++ workflow/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for camp_nanopore-quality-control."""
 
-__author__ = Michael Toomey
-__email__ = jmt4002@med.cornell.edu
-__version__ = 0.0.0
+__author__ = 'Michael Toomey', 'Lauren Mak', 'Braden Tierney', 'Berk Turhan'
+__email__ = 'jmt4002@med.cornell.edu', 'laurenmak93@gmail.com', 'btt4001@med.cornell.edu', 'bet4006@med.cornell.edu'
+__version__ = '0.0.1'
